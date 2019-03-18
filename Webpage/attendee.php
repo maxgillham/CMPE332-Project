7e8dc8f7f7d4fa947938e0f94fe2c099d33253e0
maxgillham <maxfgillham@gmail.com>
2019-03-18 15:47:52 -0400

    first few are solid

---
+++ Webpage/attendee.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="conference.css" type="text/css" rel="stylesheet" >
+</head>
+<body>
+  <h1>Attendees</h1>
+  <?php
+  echo "<table><tr><th>Student Atendees</th></tr>";
+  $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
+  $sql = "select name from attendee where type = ?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(["student"]);
+  while ($row = $stmt->fetch()) {
+	   echo "<tr><td>".$row["name"]."</td></tr>";
+  }
+  echo "<br><br><br>";
+  echo "<table><tr><th>Sponsor Atendees</th></tr>";
+  $stmt->execute(["sponsor"]);
+  while ($row = $stmt->fetch()){
+    echo "<tr><td>".$row["name"]."</td></tr>";
+  }
+  echo "<br><br><br>";
+  echo "<table><tr><th>Professional Attendees</th></tr>";
+  $stmt->execute(["professional"]);
+  while ($row = $stmt->fetch()){
+    echo "<tr><td>".$row["name"]."</td></tr>";
+  }
+  echo "<br><br><br>";
+  ?>
+</body>
+</html>
---
+++ Webpage/conference.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="conference.css" type="text/css" rel="stylesheet" >
+</head>
+<body>
+  <h1>Our Lovely Sponsors</h2>
+  <?php
+  echo "<table><tr><th>Company Name</th><th>Sponsorship Rank</th></tr>";
+  $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
+  $sql = "select company_name, sponsorship_rank from company";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  while ($row = $stmt->fetch()) {
+	   echo "<tr><td>".$row["company_name"]."</td><td>".$row["sponsorship_rank"]."</td></tr>";
+  }
+  ?>
+</body>
+</html>
